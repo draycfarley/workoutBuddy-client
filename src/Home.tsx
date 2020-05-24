ec7d667fff7e4fa4d25bfdedc9ea7ed2a9c3fd60
cf446 <cf446@cornell.edu>
2020-05-24 12:19:18 -0400

    Test backend connection via console.log

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import axios from "axios";
 
 
 class Home extends React.Component{
  
+    constructor(props: any){
+      super(props);
+    }
+
+    componentDidMount(){
+      axios.get('https://workout-buddy-server2020.herokuapp.com/workouts/all')
+            .then(function (res) {
+              console.log(res);
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+    }
 
     render(){
       return (
